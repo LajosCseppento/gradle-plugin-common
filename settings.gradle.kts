246c9b5ccf6cc7d63b5f34173d86aecae8e1d534
Lajos Cseppentő <1053931+LajosCseppento@users.noreply.github.com>
2022-07-11 01:17:08 +0200

    CI Build Matrix with Java 8, 11 and 17 (#7)
    Closes #6

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,5 +1,13 @@
 plugins {
     id("dev.lajoscseppento.ruthless") version "0.4.0"
+    id("com.gradle.enterprise") version "+"
 }
 
 rootProject.name = "gradle-plugin-common"
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
